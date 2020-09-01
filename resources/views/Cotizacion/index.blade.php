af6badc0fa1e18d9542ad31915a36966d60aacf6
Juan Felipe Garcia Duque <jfgarcia064@misena.edu.co>
2020-09-01 12:15:40 -0500

    Se  agrega  wizard a  cotiacion

--- resources/views/Cotizacion/index.blade.php
+++ resources/views/Cotizacion/index.blade.php
@@ -7,6 +7,8 @@
             <strong>LISTADO DE COTIZACIONES</strong>
                 <a class="btn btn-link" href="/cotizacion/crear">CREAR COTIZACIÓN</a>
                 <a class="btn btn-link" href=""><i class="fas fa-file-pdf"> </i> GENERAR REPORTE</a>
+                <a class="btn btn-link" href="/cotizacion/wizardModal">WIZAR</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"> Launch multistep Wizard </button> </div>
         </div>
         <div class="card-body">
             @include('flash::message')
@@ -43,6 +45,67 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Smart Wizard modal</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+            </div>
+            <div class="modal-body">
+                <div id="smartwizard">
+                    <ul>
+                        <li><a href="#step-1">Step 1<br /><small>Account Info</small></a></li>
+                        <li><a href="#step-2">Step 2<br /><small>Personal Info</small></a></li>
+                        <li><a href="#step-3">Step 3<br /><small>Payment Info</small></a></li>
+                        <li><a href="#step-4">Step 4<br /><small>Confirm details</small></a></li>
+                    </ul>
+                    <div>
+                        <div id="step-1">
+                            <div class="row">
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Name" required> </div>
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Email" required> </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Password" required> </div>
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Repeat password" required> </div>
+                            </div>
+                        </div>
+                        <div id="step-2">
+                            <div class="row">
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Address" required> </div>
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="City" required> </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="State" required> </div>
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Country" required> </div>
+                            </div>
+                        </div>
+                        <div id="step-3" class="">
+                            <div class="row">
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Card Number" required> </div>
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Card Holder Name" required> </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="CVV" required> </div>
+                                <div class="col-md-6"> <input type="text" class="form-control" placeholder="Mobile Number" required> </div>
+                            </div>
+                        </div>
+                        <div id="step-4" class="">
+                            <div class="row">
+                                <div class="col-md-12"> <span>Thanks For submitting your details with BBBootstrap.com. we will send you a confirmation email. We will review your details and revert back.</span> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('style')
+    <link href="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1581152092/smartwizard/smart_wizard.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1581152092/smartwizard/smart_wizard_theme_dots.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/modal/css/style.css') }}" rel="stylesheet">
 @endsection
 @section("scripts")
 
@@ -184,4 +247,9 @@
             });
 
     </script>
+    <script type="text/javascript" src="https://res.cloudinary.com/dxfq3iotg/raw/upload/v1581152197/smartwizard/jquery.smartWizard.min.js"></script>
+    <script src="{{ asset('assets/modal/js/modal.js') }}"></script>
 @endsection
+
+
+
