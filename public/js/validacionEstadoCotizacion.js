8d3d2cb767e57b8797f84241f2d3053537591851
Juan Felipe Garcia Duque <jfgarcia064@misena.edu.co>
2020-09-02 14:20:14 -0500

    se agregan validacions JS en modal crear estado

---
+++ public/js/validacionEstadoCotizacion.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#FrmCrearEstado").submit(function(event){
+        event.preventDefault();
+        // alert("llega");
+        let validado=0;
+
+        if ($("#Estado_Cotizacion").val() == 0) {
+
+            $("#val_estado_coti").text("*");
+            $("#val_estado_coti2").text("Debes ingresar un  estado");
+        }else{
+
+            $("#val_estado_coti").text("");
+            $("#val_estado_coti2").text("");
+            validado++;
+        }
+
+         if (validado==1)
+         {
+
+              document.FrmCrearEstado.submit();
+         }
+         else{
+            //   Swal.fire({
+            //        title:'Error en la creacion',text:'Campos pendientes por validar',icon:'error',footer:'<span class="rojo">Kreemo Solution Systems',
+            //        //width: '50%',
+            //        padding:'1rem',
+            //        //background:'#000',
+            //        backdrop:true,
+            //        //toast: true,
+            //        position:'center',
+            //   });
+              alert("Campos pendientes por validar");
+              validado = 0;
+         }
+
+    });
+});
