f9a18746781dc222387864e8f488ccc610ecbf30
Olmeiro Orozco <olme.orozco@gmail.com>
2020-10-19 11:06:26 -0500

    front users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,9 +32,9 @@ public function index(Request $request)
     ->addIndexColumn()
     ->addColumn('action', function($row){
 
-
-        $action = '<a class="btn btn-info" id="show-user" data-toggle="modal" data-id='.$row->id.'><i class="fas fa-eye"></i></a>
-        <a class="btn btn-success" id="edit-user" data-toggle="modal" data-id='.$row->id.'><i class="fas fa-edit"></i></a>
+        $action = '
+        <a class="btn btn-success" id="show-user" data-toggle="modal" data-id='.$row->id.'><i class="fas fa-eye regular"></i></a>
+        <a class="btn btn-primary" id="edit-user" data-toggle="modal" data-id='.$row->id.'><i class="fas fa-edit"></i></a>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <a id="delete-user" data-id='.$row->id.' class="btn btn-danger delete-user"><i class="fas fa-trash-alt"></i></a>';
 
@@ -51,9 +51,9 @@ public function index(Request $request)
 
 public function store(Request $request)
 {
-$uId = $request->user_id;
-User::updateOrCreate(['id' => $uId],['name' => $request->name, 'email' => $request->email, 'password'=>Hash::make($request->pw), 'rol_id'=>$request->rol]);
-return response()->json(['success'=>'Maquinaria guardada satisfactoriamente']);
+    $uId = $request->user_id;
+    User::updateOrCreate(['id' => $uId],['name' => $request->name, 'lastname' => $request->lastname,  'email' => $request->email, 'password'=>Hash::make($request->pw), 'rol_id'=>$request->rol]);
+    return response()->json(['success'=>'Maquinaria guardada satisfactoriamente']);
 }
 
 /**
@@ -83,9 +83,9 @@ public function show($id)
 
 public function edit($id)
 {
-$where = array('id' => $id);
-$user = User::where($where)->first();
-return Response::json($user);
+    $where = array('id' => $id);
+    $user = User::where($where)->first();
+    return Response::json($user);
 }
 
 /**
@@ -99,6 +99,5 @@ public function destroy($id)
 {
     $user = User::where('id',$id)->delete();
     return Response::json($user);
-    //return redirect()->route('users.index');
 }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','rol_id',
+        'name', 'lastname','email', 'password','rol_id',
     ];
 
     /**
