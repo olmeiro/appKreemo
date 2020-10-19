f99700525335ea5debc752ad83a5123647334980
Olmeiro Orozco <olme.orozco@gmail.com>
2020-10-19 17:17:26 -0500

    select obra en cotizacion

--- public/js/validacionServicio.js
+++ public/js/validacionServicio.js
@@ -331,30 +331,30 @@ function darFecha()
 
 }
 
-function validarMaquina()
-{
-    let idmaquina = $("#idmaquina").val();
-    let fechainicio = $("#fechainicio").val();
-    let fechafin = $("#fechafin").val();
-    console.log(idmaquina);
-    console.log(fechainicio);
-    console.log(fechafin);
+// function validarMaquina()
+// {
+//     let idmaquina = $("#idmaquina").val();
+//     let fechainicio = $("#fechainicio").val();
+//     let fechafin = $("#fechafin").val();
+//     console.log(idmaquina);
+//     console.log(fechainicio);
+//     console.log(fechafin);
 
     
-    $.ajax({
-        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-        url: '/servicio/validaMaquina',
-        type: 'POST',
-        data:  {
-            id: $('#idmaquina').val(),
-            fechainicio: $("#fechainicio").val(),
-            fechafin: $("#fechafin").val(),
-        },
-    }).done(function(res) {
-        console.log(res);
-        // var arreglo = JSON.parse(res);
-        //     console.log(arreglo[0].inicioBombeo)
-        //     $("#fechainicio").val(arreglo[0].inicioBombeo);
-    });
-}
+//     $.ajax({
+//         headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+//         url: '/servicio/validaMaquina',
+//         type: 'POST',
+//         data:  {
+//             id: $('#idmaquina').val(),
+//             fechainicio: $("#fechainicio").val(),
+//             fechafin: $("#fechafin").val(),
+//         },
+//     }).done(function(res) {
+//         console.log(res);
+//         // var arreglo = JSON.parse(res);
+//         //     console.log(arreglo[0].inicioBombeo)
+//         //     $("#fechainicio").val(arreglo[0].inicioBombeo);
+//     });
+// }
 
