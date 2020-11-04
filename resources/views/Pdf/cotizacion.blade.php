e9b57e029fc041881700af6c6c24d7fc42a8efb7
Olmeiro Orozco <olme.orozco@gmail.com>
2020-11-04 15:33:35 -0500

    imagen en cotizacion no da!

--- resources/views/Pdf/cotizacion.blade.php
+++ resources/views/Pdf/cotizacion.blade.php
@@ -135,10 +135,9 @@
         <p>Cotización N° {{$input["txtNumeroCotizacion"]}}</p> --}}
         <table class="vini" style="width:100%">
             <tr class="vini" >
-            {{-- <th class="foto" rowspan="3"><img src="{{asset('assets/dashboard/assets/img/1.jpg')}}" alt="Vinicol" style="width: 50px; height: 50px"></th> --}}
-            <th class="foto" rowspan="3">Vinicol</th>
 
-                <td class="vini" >PROPUESTA COMERCIAL</td>
+            <th class="foto" rowspan="3"><img src="{{ asset('http://127.0.0.1:8000/assets/dashboard/assets/img/logovinicol.jpg') }}" style="width: 150px; height: 100px"></th>
+            <td class="vini" >PROPUESTA COMERCIAL</td>
                 <td class="vini" >VERSIÓN 001</td>
             </tr>
             <tr>
